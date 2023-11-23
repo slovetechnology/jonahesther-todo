6d2abf1232cc689ad378bf5564b91e8541605636
slovetechnology <slovetech619@gmail.com>
2023-11-23 14:53:10 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,349 @@
+// import Image from 'next/image'
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+//         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+//           Get started by editing&nbsp;
+//           <code className="font-mono font-bold">app/page.tsx</code>
+//         </p>
+//         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+//           <a
+//             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             By{' '}
+//             <Image
+//               src="/vercel.svg"
+//               alt="Vercel Logo"
+//               className="dark:invert"
+//               width={100}
+//               height={24}
+//               priority
+//             />
+//           </a>
+//         </div>
+//       </div>
+
+//       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+//         <Image
+//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+//           src="/next.svg"
+//           alt="Next.js Logo"
+//           width={180}
+//           height={37}
+//           priority
+//         />
+//       </div>
+
+//       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+//         <a
+//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={`mb-3 text-2xl font-semibold`}>
+//             Docs{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+//             Find in-depth information about Next.js features and API.
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={`mb-3 text-2xl font-semibold`}>
+//             Learn{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+//             Learn about Next.js in an interactive course with&nbsp;quizzes!
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={`mb-3 text-2xl font-semibold`}>
+//             Templates{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+//             Explore starter templates for Next.js.
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={`mb-3 text-2xl font-semibold`}>
+//             Deploy{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+//             Instantly deploy your Next.js site to a shareable URL with Vercel.
+//           </p>
+//         </a>
+//       </div>
+//     </main>
+//   )
+// }
+
+import React from 'react'
+import bgimg from '../public/images/header.jpg'
+import bgimg4 from '../public/images/imgs1.jpg'
+import bgimg2 from '../public/images/image92.png'
+import bgimg3 from '../public/images/imgs11.png'
+import { SlMagnifier } from 'react-icons/sl'
+import { FaPlay } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
+import { ProductList1, ProductList2, ProductList3, ProductList4, Secondection, TopLists } from '@/components/general/Extras'
+
+const page = () => {
+  return (
+    <div>
+      <div className="">
+        {/* first section */}
+        <div
+          className='banner'
+          style={{ backgroundImage: `url(${bgimg.src})` }}
+        >
+          <div className="bannerdiv h-full flex items-center justify-center flex-col">
+            <h1 className='text-white font-semibold text-5xl text-center'>Welcome to Shipam: your Gatway <br /> to Versatile E-Comerce</h1>
+            <div className="bg-slate-100/40 p-6 mt-10 rounded-full w-11/12 max-w-[46rem]">
+              <div className="bg-white flex items-center justify-between p-1.5 rounded-full">
+                <input type="text" className='p-2 rounded-full outline-none border-none' placeholder='What are you looking for?' />
+                <button className="bg-red-600 text-red-100 text-xl rounded-full p-3"> <SlMagnifier /> </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* second section */}
+        <div className="p-5 bg-[#343434]">
+          <div className="w-10/12 mx-auto py-8">
+            <div className="lg:gap-10 grid grid-cols-1 lg:grid-cols-3">
+              <div className="flex flex-col gap-5 col-span-2">
+                <h2 className='text-slate-100 text-5xl leading-[3.5rem] w-[95%] font-semibold'>We are not just an e-commerce platform, we are your gateway to a world of possilibities</h2>
+                <Link href="/" className='bg-red-700 text-red-100 py-3 rounded-full w-[13rem] text-center text-xl'>Join Now</Link>
+              </div>
+              <div className="w-fit ml-auto col-span-1">
+                <Image
+                  src="/images/img2.png"
+                  alt="Shipam"
+                  width={350}
+                  height={100}
+                />
+              </div>
+            </div>
+            <div className="">
+              <div className="text-slate-100 text-2xl font-semibold my-4">Three Types of Users, One Unified Platform</div>
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                {TopLists.map(((item, index) => (
+                  <div className="bg-[#4b4c4d] p-4 rounded-lg" key={index}>
+                    <h3 className='text-lg w-11/12 font-semibold text-slate-200 mb-3'>{item.title}</h3>
+                    <ul>
+                      {item.subs.map((ele, i) => (
+                        <ul className="flex gap-1 mb-2 text-slate-300 text-xs" key={i}> <div className="p-1 bg-slate-200 w-fit h-fit rounded-full mt-2"></div> {ele}</ul>
+                      ))}
+                    </ul>
+                  </div>
+                )))}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* third section */}
+        <div className="bg-[#f4f4f4]">
+          <div className="w-10/12 mx-auto p-8">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <div className="h-full flex flex-col justify-center lg:w-10/12 gap-8">
+                <h2 className='font-semibold text-5xl'>What you can do with Shipam</h2>
+                <p>Shipam simplifies your selling process, ensuring a seamless shopping experience for buyers. Our platform makes bulk ordering a breeze and facilitates effective communication between users, fostering transparency and smooth negotiations.</p>
+              </div>
+              <div className="">
+                <div className="grid grid-cols-2 gap-5">
+                  {Secondection.map((item, i) => (
+                    <div className=" bg-white rounded-xl px-5 py-6 shadow-2xl" key={i}>
+                      <div className="bg-red-100 rounded-full p-3 w-fit">
+                        <Image
+                          src={item.img}
+                          alt="Shipam"
+                          width={33}
+                          height={100}
+                        />
+                      </div>
+                      <div className="font-semibold text-2xl my-3">{item.title}</div>
+                      <div className="text-sm">{item.text}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* fourth section */}
+        <div className="py-8">
+          <div className="w-10/12 mx-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-5 mb-10">
+              <div className="col-span-1">
+                <div className="w-full p-0 h-full bg-cover bg-center bg-no-repeat"
+                  style={{ backgroundImage: `url(${bgimg2.src})` }}
+                >
+                  <div className="px-4 pt-10">
+                    <div className="font-semibold mb-6">Home and outdoor</div>
+                    <Link href="/" className='bg-white py-2.5 px-3 rounded-lg text-xs font-semibold shadow-md'>Source now</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-4">
+                <div className="grid grid-cols-2 lg:grid-cols-4">
+                  {ProductList1.map((item, i) => (
+                    <div className="relative border p-3 h-[8rem]" key={i}>
+                      {item.status && <div className="absolute top-0 right-0 bg-red-700 text-white capitalize py-1 px-3 text-[0.6rem]">new</div>}
+                      <div className="mb-4">{item.title}</div>
+                      <div className="text-xs text-slate-500 capitalize">{item.tag}</div>
+                      <div className="text-xs text-slate-500">{item.pricing}</div>
+                      <div className="absolute bottom-2 right-2">
+                        <Image
+                          src={item.img}
+                          alt="Shipam"
+                          width={70}
+                          height={100}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-5 mb-10">
+              <div className="col-span-1">
+                <div className="w-full p-0 h-full bg-cover bg-center bg-no-repeat"
+                  style={{ backgroundImage: `url(${bgimg3.src})` }}
+                >
+                  <div className="px-4 pt-10">
+                    <div className="font-semibold mb-12 w-3/4">Consumer electornics and gadgets</div>
+                    <Link href="/" className='bg-white py-2.5 px-3 rounded-lg text-xs font-semibold shadow-md'>Source now</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-4">
+                <div className="grid grid-cols-2 lg:grid-cols-4">
+                  {ProductList2.map((item, i) => (
+                    <div className="relative border p-3 h-[9rem]" key={i}>
+                      {item.status && <div className="absolute top-0 right-0 bg-red-700 text-white capitalize py-1 px-3 text-[0.6rem]">new</div>}
+                      <div className="mb-4">{item.title}</div>
+                      <div className="text-xs text-slate-500 capitalize">{item.tag}</div>
+                      <div className="text-xs text-slate-500">{item.pricing}</div>
+                      <div className="absolute bottom-2 right-2">
+                        <Image
+                          src={item.img}
+                          alt="Shipam"
+                          width={70}
+                          height={100}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-center gap-3 font-semibold text-3xl">Explore all our product offerings <div className="flex items-center mt-2 text-base"><FaPlay className="text-red-700" /><FaPlay className="text-red-700" /></div> </div>
+          </div>
+        </div>
+        {/* fifth section */}
+        <div className="bg-[#f4f4f4] py-6">
+          <div className="w-10/12 mx-auto">
+            <div className="font-semibold text-[2rem]">Recommended items</div>
+            <div className="grid grid-cols-2 lg:grid-cols-5 gap-5">
+              {ProductList3.map((item, i) => (
+                <div className="bg-white relative px-4 py-9" key={i}>
+                  {item.discount && <div className="absolute top-24 right-0 bg-red-700 text-white text-[0.53rem] py-1 px-3">{item.discount}</div>}
+                  <div className="w-full relative mx-auto h-[8rem]">
+                    <Image
+                      src={item.img}
+                      alt="Shipam"
+                      fill
+                      objectFit='contain'
+                    />
+                  </div>
+                  <div className="text-xs my-3 font-medium">{item.pricing}</div>
+                  <div className="text-xs text-slate-400">{item.title}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        
+        {/* sixth section */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 bg-[#f4f4f4] py-14">
+          <div className=" w-10/12 ml-auto flex flex-col justify-center gap-7">
+            <h2 className="text-5xl font-semibold w-4/6">What Else We Offer</h2>
+            <p className='lg:w-4/6 text-sm'>Join Shipam for streamlined, transparent and thriving commerce - redefining how business is conducted.</p>
+          </div>
+          <div className="bg-white shadow-xl rounded-bl-3xl rounded-tl-3xl">
+            <div className="grid grid-cols-2 w-10/12 py-10 pl-5 gap-6">
+              {ProductList4.map((item, i) => (
+                <div className="flex items-center gap-3" key={i}>
+                  <div className="bg-red-100 w-fit rounded-lg p-3">
+                    <Image
+                      src={item.img}
+                      alt="Shipam"
+                      width={45}
+                      height={50}
+                    />
+                  </div>
+                  <div className="font-semibold text-lg text-slate-700">{item.title}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* seventh section */}
+        <div className="h-[40rem] bg-cover bg-center"
+          style={{ backgroundImage: `url(${bgimg4.src})` }}
+        >
+          <div className="bg-black/50 h-full py-10 flex items-center justify-center flex-col">
+            <div className="w-10/12 mx-auto">
+              <div className="text-white text-center text-5xl font-semibold mb-20">We're committed  to making your e-commerce experience as versatile and efficient as possible.</div>
+              <div className="bg-white/30 rounded-2xl flex flex-col gap-10 items-center justify-center p-16 w-10/12 mx-auto">
+                <div className="font-semibold text-3xl text-white">Discover the Future of Commerce with Shipam</div>
+                <div className="text-white text-center">At Shipam, we've redefined the e-commerce landscape, creating a dynamic marketplace that adapts to the diverse needs of businesses, vendors, and consumers. More than just an online shopping platform, Shipam is a complete ecosystem, facilitating B2B (Business-to-Business), P2P (Peer-to-Peer), and B2C (Business-to-Consumer) interactions in one unified space.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* seventh section */}
+        <div className="w-10/12 mx-auto my-20">
+          <div className="text-5xl text-center font-semibold">How to Use Shipam</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/general/Extras.ts
@@ -0,0 +1,250 @@
+
+export const TopLists = [
+    {
+        title: `For Business (B2B):`,
+        subs: [
+            `Manufacturers, wholesalers, distributors, and retailers looking to connect with other businesses.`,
+            `Source raw materials, products, or services.`,
+            `Establish partnership and streamline your supply chain.`
+        ]
+    },
+    {
+        title: `For Individual Sellers and Service Providers (P2P):`,
+        subs: [
+            `Enterpreneurs, freelancers and independent sellers with unique offerings.`,
+            `List and sell products or services directly to other users or customers.`,
+        ]
+    },
+    {
+        title: `For Customers (B2C):`,
+        subs: [
+            `Everyday customers seeking a wide range of products and services.`,
+            `Browse, compare and purchase products directly from sellers or service providers`,
+        ]
+    },
+]
+// #B90404
+export const Secondection = [
+    {
+        img: `/images/img1.svg`,
+        title: `List and Sell with Ease`,
+        text: `Sellers can effortlessly create detailed product listings with images and pricing.`
+    },
+    {
+        img: `/images/img3.svg`,
+        title: `Seamless Shopping Experience`,
+        text: `Buyers can search, filter and explore product listings.`
+    },
+    {
+        img: `/images/img2.svg`,
+        title: `Bulk Ordering Made Simple`,
+        text: `Buyers can place bulk order requests for multple sellers to fulfill simultaneously with time restricted order processing.`
+    },
+    {
+        img: `/images/img4.svg`,
+        title: `Effective Communication`,
+        text: `Our integrated messaging system facilitates easy and efficient communication between buyers and sellers.`
+    }
+]
+
+
+export const ProductList1 = [
+    {
+        img: `/images/imgs20.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/image94.png`,
+        title: `Sofa and chair`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/image93.png`,
+        title: `Kitchen dishes`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/image90.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+    {
+        img: `/images/image88.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/image87.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+    {
+        img: `/images/imgs21.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/imgs22.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+]
+
+
+export const ProductList2 = [
+    {
+        img: `/images/imgs15.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/imgs17.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+    {
+        img: `/images/imgs14.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/imgs13.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+    {
+        img: `/images/imgs18.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/imgs16.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: true
+    },
+    {
+        img: `/images/image32.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+    {
+        img: `/images/image33.png`,
+        title: `Soft chairs`,
+        pricing: `USD 19`,
+        tag: `from`,
+        status: false
+    },
+]
+
+export const ProductList3 = [
+    {
+        img: `/images/imgs10.jpg`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: `-20%`
+    },
+    {
+        img: `/images/imgs9.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: null
+    },
+    {
+        img: `/images/imgs8.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: `-20%`
+    },
+    {
+        img: `/images/imgs7.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: null
+    },
+    {
+        img: `/images/imgs6.jpg`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: `-20%`
+    },
+    {
+        img: `/images/imgs5.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: null
+    },
+    {
+        img: `/images/imgs4.jpg`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: `-20%`
+    },
+    {
+        img: `/images/imgs3.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: null
+    },
+    {
+        img: `/images/image90.png`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: `-20%`
+    },
+    {
+        img: `/images/imgs2.jpg`,
+        title: `T-shirts with multiple colors, for men`,
+        pricing: `$10.30`,
+        discount: null
+    },
+]
+
+export const ProductList4 = [
+    {
+        img: `/images/search.svg`,
+        title: `Precision Searching`
+    },
+    {
+        img: `/images/inventory_2.svg`,
+        title: `Negotiation at Your Fingertips`
+    },
+    {
+        img: `/images/send.svg`,
+        title: `Streamlined Logistics`
+    },
+    {
+        img: `/images/img5.svg`,
+        title: `Community Feedback`
+    },
+]
